29be9dd152442f36600ce00fde7129377901cce6
LuisVenturaH <l_ventur@yahoo.com>
2023-12-05 12:08:26 +0100

    Login and registro updated

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,22 @@
 const host = "http://localhost:8000";
 
-// Fucnion para crear nuevos registros
-function registroNuevoUsario (){
+// Funcion para crear nuevos registros (PROBADO Y FUNCIONA CORRECTAMENTE)
+function registroNuevoUsuario() {
     const nombre = document.getElementById("nombre").value;
     const apellidos = document.getElementById("apellidos").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    
+
     if (nombre === "" || apellidos === "" || email === "" || password === "") {
-        alert("Has dejado algun campo en blanco, favor completarlo para proceder el registro");
-    }
-    else{
+        alert("Has dejado algún campo en blanco, favor completarlo para proceder con el registro");
+    } else {
         alert("Registro completado correctamente");
-        window.location.href = "http://localhost:8000/html/index.html"
+        window.location.href = "http://localhost:8000/html/index.html";
 
         fetch(`${host}/nuevo_registro`, {
             method: "POST",
             headers: {
-                "Content-Type": "applicaction/json"
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({
                 nombre: nombre,
@@ -26,31 +25,48 @@ function registroNuevoUsario (){
                 password: password
             })
         })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error("Error en la solicitud:", error);
+        });
     }
 }
 
-// Funcion para login
-function login (){
+
+// Funcion para login (PROBADO Y FUNCIONA CORRECTAMENTE)
+function login() {
     const usuario = document.getElementById("usuario").value;
     const contrasena = document.getElementById("contrasena").value;
-    
+
     if (usuario === "" || contrasena === "") {
         alert("No has introducido usuario o contraseña correctamente. Vuelve a intentarlo");
-    }
-    else{
-        alert("registro correcto");
-        localStorage.setItem("Usuario", usuario)
-        window.location.href = "http://localhost:8000/html/index.html"
+    } else {
+        alert("Registro correcto");
+        localStorage.setItem("Usuario", usuario);
 
+        // Redirecciona después de guardar en localStorage
+        window.location.href = "http://localhost:8000/html/index.html";
+
+        // Hacer la solicitud fetch para autenticación
         fetch(`${host}/login`, {
             method: "POST",
             headers: {
-                "Content-Type": "applicaction/json"
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({
                 email: usuario,
                 password: contrasena
             })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // Puedes manejar la respuesta de la API aquí si es necesario
+        })
+        .catch(error => {
+            console.error("Error en la solicitud:", error);
         });
     }
 }
